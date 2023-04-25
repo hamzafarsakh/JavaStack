91ea59a4a6eb8df0b125973ad1678444c4f47b43
unknown <hamza.m.farsakh5@gmail.com>
2023-04-25 11:22:35 +0200

    Map of the Hashmatique

--- Java Fundamentals/Map of the Hashmatique/Hellow.java
+++ Java Fundamentals/Map of the Hashmatique/Hellow.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.util.HashMap;
 public class Hellow{
     HashMap<String, String> trackList = new HashMap<String, String>();
-    trackList.put("nninja@codingdojo.com", "Nancy Ninja");
+    
     // trackList.put("aaaaa","1111111");
     // trackList.put("bbbbbbbb","2222222");
     // trackList.put("cccccc","333333333");
--- Java Fundamentals/Map of the Hashmatique/HellowTest.java
+++ Java Fundamentals/Map of the Hashmatique/HellowTest.java
@@ -1,8 +1,20 @@
 
 import java.util.*;
 import java.util.HashMap;
+import java.util.Set;
 public class HellowTest{
     public static void main(String[] args){
         Hellow x = new Hellow();
+
+        x.trackList.put("aaaaa","1111111");
+        x.trackList.put("bbbbbbbb","2222222");
+        x.trackList.put("cccccc","333333333");
+        x.trackList.put("dddddd","4444444444");
+        System.out.println(x.trackList.get("bbbbbbbb"));
+
+        Set<String> keys = x.trackList.keySet();
+        for(String key : keys){
+            System.out.println(key+": "+x.trackList.get(key));
+        }
     }
 }
