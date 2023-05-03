71b81e1eec7303d67ae370229e4e31b51b5bf7cb
unknown <hamza.m.farsakh5@gmail.com>
2023-05-03 21:58:08 +0300

    Rendering Books

--- Java Spring/Rendering Books/demo/src/main/java/com/example/demo/controllers/BookController.java
+++ Java Spring/Rendering Books/demo/src/main/java/com/example/demo/controllers/BookController.java
@@ -3,8 +3,8 @@ package com.example.demo.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.models.Book;
 import com.example.demo.services.BookService;
@@ -14,10 +14,8 @@ public class BookController {
 	@Autowired
 	BookService bookService;
 	
-	@RequestMapping("/books/{bookId}")
-	public String show(Model model, @PathVariable("bookID") Long bookId
-			
-			) {
+	@GetMapping("/books/{bookId}")
+	public String show(Model model, @PathVariable("bookId") Long bookId) {
 		Book book = bookService.findBook(bookId);
 		model.addAttribute("book", book);
 		return "show.jsp";
--- Java Spring/Rendering Books/demo/src/main/java/com/example/demo/repository/BookRepository.java
+++ Java Spring/Rendering Books/demo/src/main/java/com/example/demo/repository/BookRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -18,5 +19,7 @@ public interface BookRepository extends CrudRepository<Book, Long>{
  Long countByTitleContaining(String search);
  // this method deletes a book that starts with a specific title
  Long deleteByTitleStartingWith(String search);
+ 
+ Optional<Book> findById(Long id);
 }
 
