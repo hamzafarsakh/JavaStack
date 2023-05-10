ba7d1b34e8ef03236c973f162ffa79f02d87fb43
unknown <hamza.m.farsakh5@gmail.com>
2023-05-10 08:42:44 +0300

    new

---
+++ Java Spring/Book Broker/demo/src/main/java/com/example/demo/models/Book.java
@@ -0,0 +1,169 @@
+package com.example.demo.models;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+
+@Entity
+@Table(name="books") //change the table name
+public class Book {  //change the class name
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @NotEmpty
+    @Size(min=3, max=30, message="Username must be between 3 and 30 characters")
+    private String title;
+    
+    @NotEmpty
+    @Size(min=3, max=30)
+    private String author;
+    @NotEmpty
+    @Size(min=3, max=100)
+    private String description;
+   
+    
+    @Column(updatable=false)
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date createdAt;
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date updatedAt;
+    
+    @ManyToOne(fetch = FetchType.LAZY) 
+    @JoinColumn(name="user_id")
+    private User user;
+    
+    @ManyToOne(fetch = FetchType.LAZY) 
+    @JoinColumn(name="userFav_id")
+    private User userFav;
+
+    
+    public Book() {} 
+
+    
+    
+	public User getUserFav() {
+		return userFav;
+	}
+
+
+
+	public void setUserFav(User userFav) {
+		this.userFav = userFav;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+
+	public String getAuthor() {
+		return author;
+	}
+
+
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+
+
+	public User getUser() {
+		return user;
+	}
+
+
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+
+
+	@PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Date();
+    }
+	@PrePersist
+	protected void onCreate(){
+		this.createdAt = new Date();
+	}
+  
+}
+    
+
+
